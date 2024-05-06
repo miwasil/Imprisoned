dbd2754eaf4ead855fbb3a705040300d66957903
kasikora <115181450+kasikora@users.noreply.github.com>
2024-05-06 22:06:11 +0200

    new creature, patroling and chasin
    mimic, patroling and chasing

---
+++ Assets/Mimic/Scripts/Movement.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+namespace MimicSpace
+{
+    /// <summary>
+    /// This is a very basic movement script, if you want to replace it
+    /// Just don't forget to update the Mimic's velocity vector with a Vector3(x, 0, z)
+    /// </summary>
+    public class Movement : MonoBehaviour
+    {
+        [Header("Controls")]
+        [Tooltip("Body Height from ground")]
+        [Range(0.5f, 5f)]
+        public float height = 0.8f;
+        public float speed = 5f;
+        Vector3 velocity = Vector3.zero;
+        public float velocityLerpCoef = 4f;
+        Mimic myMimic;
+        
+        //
+        public NavMeshAgent agent;
+
+        public Transform player;
+
+        public LayerMask whatIsGround, whatIsPlayer;
+        
+        //Patroling
+        public Vector3 walkPoint;
+        bool walkPointSet;
+        public float walkPointRange;
+
+        //Attacking
+        public float timeBetweenAttacks;
+        bool alreadyAttacked;
+        public GameObject projectile;
+
+        //States
+        public float sightRange, attackRange;
+        public bool playerInSightRange, playerInAttackRange;
+
+        private void Awake()
+        {
+            player = GameObject.Find("RigidBodyFPSController").transform;
+            agent = GetComponent<NavMeshAgent>();
+        }
+        private void Start()
+        {
+            myMimic = GetComponent<Mimic>();
+        }
+
+        void Update()
+        {
+            velocity = Vector3.Lerp(velocity, new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * speed, velocityLerpCoef * Time.deltaTime);
+
+            // Assigning velocity to the mimic to assure great leg placement
+            myMimic.velocity = velocity;
+            
+            //Check for sight and attack range
+            playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+            playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+            if (!playerInSightRange && !playerInAttackRange) Patroling();
+            if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+            //if (playerInAttackRange && playerInSightRange) AttackPlayer();
+            /*transform.position = transform.position + velocity * Time.deltaTime;
+            RaycastHit hit;
+            Vector3 destHeight = transform.position;
+            if (Physics.Raycast(transform.position + Vector3.up * 5f, -Vector3.up, out hit))
+                destHeight = new Vector3(transform.position.x, hit.point.y + height, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, destHeight, velocityLerpCoef * Time.deltaTime);*/
+        }
+        private void Patroling()
+        {
+            if (!walkPointSet) SearchWalkPoint();
+
+            if (walkPointSet)
+                agent.SetDestination(walkPoint);
+
+            Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+            //Walkpoint reached
+            if (distanceToWalkPoint.magnitude < 1f)
+                walkPointSet = false;
+        }
+        private void SearchWalkPoint()
+        {
+            //Calculate random point in range
+            float randomZ = Random.Range(-walkPointRange, walkPointRange);
+            float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+            walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+            if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+                walkPointSet = true;
+        }
+
+        private void ChasePlayer()
+        {
+            agent.SetDestination(player.position);
+        }
+
+    }
+
+}
